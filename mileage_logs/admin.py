21fb5ba7109c0a9733ab04695408db7e92072596
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-05-31 19:08:35 +0000

    add mileage log to admin

--- mileage_logs/admin.py
+++ mileage_logs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MileageLog, MileageLogEntry
 
 # Register your models here.
+admin.site.register(MileageLog)
+admin.site.register(MileageLogEntry)
