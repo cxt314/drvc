4c752f42effd30ccb284c72fefdabea02ce84830
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-05-31 18:40:51 +0000

    add unique member name field

--- members/models.py
+++ members/models.py
@@ -1,13 +1,18 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+def generate_unique_code():
+    return str(uuid.uuid4())[:8]
+
 class Member(models.Model):
-    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=50, unique=True, default=generate_unique_code)
+    email = models.EmailField(unique=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.email
+        return self.name
 
 class MemberAlias(models.Model):
     name = models.CharField(max_length=100, unique=True)
