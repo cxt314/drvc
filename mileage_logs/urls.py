06259810ef2f668af225b2463aa44271e76220d2
cxt314 <103610756+cxt314@users.noreply.github.com>
2025-08-12 00:33:01 +0000

    refactoring mileage log entry flow

--- mileage_logs/urls.py
+++ mileage_logs/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # MileageLog list view
-    path('', views.MileageLogListView.as_view(), name='list_mileage_logs'),
+    path('', views.MileageLogListView.as_view(), name='mileage_logs_list'),
     # MileageLog detail view
     path('<int:pk>/', views.MileageLogDetailView.as_view(), name='detail_mileage_log'),
     # URL to create a new mileage log
--- mileage_logs/views.py
+++ mileage_logs/views.py
@@ -25,29 +25,21 @@ class MileageLogDetailView(DetailView):
 
 def create_mileage_log_view(request):
     """
-    View to create a new MonthlyMileageLog and its nested inlines.
+    View to create a new MonthlyMileageLog.
     """
-    monthly_log_form = MonthlyMileageLogForm(request.POST or None)
-    formset = MileageLogEntryFormSet(request.POST or None, request.FILES or None)
+    form = MonthlyMileageLogForm(request.POST or None)
 
     if request.method == 'POST':
-        if monthly_log_form.is_valid() and formset.is_valid():
-            monthly_log = monthly_log_form.save(commit=False)
+        if form.is_valid():
+            monthly_log = form.save(commit=False)
             monthly_log.save()
-
-            instances = formset.save(commit=False)
-            for instance in instances:
-                instance.monthly_log = monthly_log
-                instance.save()
-            formset.save_m2m()
 
             return redirect('list_mileage_logs')  # Redirect to the list view after creation
 
     context = {
-        'monthly_log_form': monthly_log_form,
-        'formset': formset,
+        'monthly_log_form': form,
     }
-    return render(request, 'mileage_logs/add_mileage_log.html', context)
+    return render(request, 'mileage_logs/mileage_log_edit.html', context)
 
 def update_mileage_log_view(request, pk):
     """
@@ -56,44 +48,93 @@ def update_mileage_log_view(request, pk):
     monthly_log = get_object_or_404(MonthlyMileageLog, pk=pk)
 
     if request.method == 'POST':
-        formset = MileageLogEntryFormSet(request.POST, request.FILES, instance=monthly_log)
+        form = MonthlyMileageLogForm(request.POST, instance=monthly_log)
         
-        # We need to process the nested formsets as well.
-        # This is a bit more complex, but here's a basic structure.
-        claim_formsets = []
-        if formset.is_valid():
-            # Save parent forms first
-            instances = formset.save(commit=False)
-            
-            # Loop through all forms in the formset
-            for form in formset.ordered_forms:
-                if form.instance.pk:
-                    # If it's an existing instance, process its claim formset.
-                    claim_formset = MileageClaimFormSet(request.POST, request.FILES, instance=form.instance, prefix=f'claims_{form.instance.pk}')
-                    if claim_formset.is_valid():
-                        claim_formset.save()
-                    claim_formsets.append(claim_formset)
-            
-            # Save parent instances
-            formset.save()
-            return redirect('success_page') # Replace with your success URL
+        if form.is_valid():
+            monthly_log = form.save(commit=False)
+            monthly_log.save()
+            return redirect('detail_mileage_log', pk=monthly_log.pk) # Replace with your success URL
     
     else: # GET request
-        formset = MileageLogEntryFormSet(instance=monthly_log)
-        
-        # Auto-populate the start_mileage for the new form
-        # This is the server-side logic you requested.
-        last_entry = monthly_log.log_entries.order_by('-entry_date', '-start_mileage').first()
-        if last_entry and formset.forms:
-            # Find the empty form in the formset.
-            # The last form is usually the empty one provided by `extra=1`.
-            empty_form = formset.forms[-1]
-            if not empty_form.instance.pk:
-                empty_form.initial['start_mileage'] = last_entry.end_mileage
+        form = MonthlyMileageLogForm(instance=monthly_log)
         
     context = {
-        'formset': formset,
-        'monthly_log': monthly_log,
+        'monthly_log_form': form,
     }
     
-    return render(request, 'mileage_logs/update_mileage.html', context)
+    return render(request, 'mileage_logs/mileage_log_edit.html', context)
+
+# def create_mileage_log_view(request):
+#     """
+#     View to create a new MonthlyMileageLog and its nested inlines.
+#     """
+#     monthly_log_form = MonthlyMileageLogForm(request.POST or None)
+#     #formset = MileageLogEntryFormSet(request.POST or None, request.FILES or None)
+
+#     if request.method == 'POST':
+#         if monthly_log_form.is_valid() and formset.is_valid():
+#             monthly_log = monthly_log_form.save(commit=False)
+#             monthly_log.save()
+
+#             instances = formset.save(commit=False)
+#             for instance in instances:
+#                 instance.monthly_log = monthly_log
+#                 instance.save()
+#             formset.save_m2m()
+
+#             return redirect('list_mileage_logs')  # Redirect to the list view after creation
+
+#     context = {
+#         'monthly_log_form': monthly_log_form,
+#         'formset': formset,
+#     }
+#     return render(request, 'mileage_logs/add_mileage_log.html', context)
+
+# def update_mileage_log_view(request, pk):
+#     """
+#     View to update a MonthlyMileageLog and its nested inlines.
+#     """
+#     monthly_log = get_object_or_404(MonthlyMileageLog, pk=pk)
+
+#     if request.method == 'POST':
+#         formset = MileageLogEntryFormSet(request.POST, request.FILES, instance=monthly_log)
+        
+#         # We need to process the nested formsets as well.
+#         # This is a bit more complex, but here's a basic structure.
+#         claim_formsets = []
+#         if formset.is_valid():
+#             # Save parent forms first
+#             instances = formset.save(commit=False)
+            
+#             # Loop through all forms in the formset
+#             for form in formset.ordered_forms:
+#                 if form.instance.pk:
+#                     # If it's an existing instance, process its claim formset.
+#                     claim_formset = MileageClaimFormSet(request.POST, request.FILES, instance=form.instance, prefix=f'claims_{form.instance.pk}')
+#                     if claim_formset.is_valid():
+#                         claim_formset.save()
+#                     claim_formsets.append(claim_formset)
+            
+#             # Save parent instances
+#             formset.save()
+#             return redirect('success_page') # Replace with your success URL
+    
+#     else: # GET request
+#         formset = MileageLogEntryFormSet(instance=monthly_log)
+        
+#         # Auto-populate the start_mileage for the new form
+#         # This is the server-side logic you requested.
+#         last_entry = monthly_log.log_entries.order_by('-entry_date', '-start_mileage').first()
+#         if last_entry and formset.forms:
+#             # Find the empty form in the formset.
+#             # The last form is usually the empty one provided by `extra=1`.
+#             empty_form = formset.forms[-1]
+#             if not empty_form.instance.pk:
+#                 empty_form.initial['start_mileage'] = last_entry.end_mileage
+        
+#     context = {
+#         'formset': formset,
+#         'monthly_log': monthly_log,
+#     }
+    
+#     return render(request, 'mileage_logs/update_mileage.html', context)
